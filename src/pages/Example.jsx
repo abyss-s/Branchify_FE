14b67dbd2174dba8b61edf10365b5074c6d41f65
abyss-s <xyz@inha.edu>
2024-11-06 04:18:04 +0900

    ✨ Feat: router 생성 및 Example 페이지에서 테스트

---
+++ src/pages/Example.jsx
@@ -0,0 +1,10 @@
+const Example = () => {
+  return (
+    <>
+      <h1>Test</h1>
+      레포 초기 세팅 테스트
+    </>
+  );
+};
+
+export default Example;
---
+++ src/routes.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "./components/Layout";
+import Example from "./pages/Example";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      // 추가 라우팅
+      {
+        path: "example",
+        element: <Example />,
+      },
+    ],
+  },
+]);
+
+export default router;
