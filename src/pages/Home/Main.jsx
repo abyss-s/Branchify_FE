b1b4ae6100955b4cd0ffc3b7cf4c57af45714b8f
영 <77565980+abyss-s@users.noreply.github.com>
2024-11-11 02:21:11 +0900

    ✨ Feat: 메인 섹션 오른쪽 페이지 구현

--- src/pages/Home/Main.jsx
+++ src/pages/Home/Main.jsx
@@ -1,12 +1,30 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import markIcon from '../../assets/icons/mark.png';
+import msg1 from '../../assets/images/message-1.png';
+import msg2 from '../../assets/images/message-2.png';
+import msg3 from '../../assets/images/message-3.png';
 
 const mainStyle = css`
   display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+
+  @media (max-width: 1000px) {
+    flex-direction: column;
+    h1 {
+      font-size: 40px;
+    }
+    p {
+      font-size: 18px;
+    }
+  }
+`;
+
+const lnbStyle = css`
+  display: flex;
   flex-direction: column;
   width: 594px;
-  height: 273px;
 `;
 
 const h1Style = css`
@@ -42,30 +60,78 @@ const pStyle = css`
   padding-top: 20px;
 `;
 
+const rnbStyle = css`
+  display: flex;
+  flex-direction: column;
+  width: 402px;
+  position: relative; /* 부모 요소에 위치 설정 */
+  margin-top: 50px;
+`;
+
+const imgStyle = css`
+  position: absolute;
+  opacity: 0;
+  animation: fadeIn 0.5s forwards;
+
+  :nth-of-type(1) {
+    animation-delay: 0s;
+    z-index: 3;
+    top: 0;
+    left: 0;
+  }
+  :nth-of-type(2) {
+    animation-delay: 0.5s;
+    z-index: 2;
+    top: 50px;
+    left: 25px;
+  }
+  :nth-of-type(3) {
+    animation-delay: 1s;
+    z-index: 1;
+    top: 100px;
+    left: 50px;
+  }
+
+  @keyframes fadeIn {
+    to {
+      opacity: 1;
+    }
+  }
+`;
+
 const Main = () => {
   return (
-    <div css={mainStyle}>
-      <img
-        src={markIcon}
-        alt=""
-        css={css`
-          width: 40.192px;
-          height: 56.271px;
-          flex-shrink: 0;
-        `}
-      />
-      <h1 css={h1Style}>비개발자를 위한 협업 관리 도구</h1>
-      <h1 css={h1Style}>
-        <span css={spanStyle}>
-          <span className="highlight">B</span>ranchify
-        </span>{' '}
-        입니다.
-      </h1>
-      <p css={pStyle}>
-        개발자와 소통이 되지 않아 답답했던 과거는 이제 안녕! <br />
-        이제는 우리 프로젝트의 진행상황과 내용을 제대로 이해할 시간이에요.
-      </p>
-    </div>
+    <section css={mainStyle}>
+      {/* 왼쪽 */}
+      <div css={lnbStyle}>
+        <img
+          src={markIcon}
+          alt=""
+          css={css`
+            width: 40.192px;
+            height: 56.271px;
+            flex-shrink: 0;
+          `}
+        />
+        <h1 css={h1Style}>비개발자를 위한 협업 관리 도구</h1>
+        <h1 css={h1Style}>
+          <span css={spanStyle}>
+            <span className="highlight">B</span>ranchify
+          </span>{' '}
+          입니다.
+        </h1>
+        <p css={pStyle}>
+          개발자와 소통이 되지 않아 답답했던 과거는 이제 안녕! <br />
+          이제는 우리 프로젝트의 진행상황과 내용을 제대로 이해할 시간이에요.
+        </p>
+      </div>
+      {/* 오른쪽 */}
+      <div css={rnbStyle}>
+        <img src={msg1} alt="" css={imgStyle} />
+        <img src={msg2} alt="" css={imgStyle} />
+        <img src={msg3} alt="" css={imgStyle} />
+      </div>
+    </section>
   );
 };
 
