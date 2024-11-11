470c884fba58b86781fcfe7da22f7a93eebf8b35
영 <77565980+abyss-s@users.noreply.github.com>
2024-11-11 23:15:18 +0900

    ✨ Feat: 파이 소개 페이지 구현

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@
 import { css } from '@emotion/react';
 import Main from './Main';
 import Section2 from './Section2';
+import Fy from './Fy';
 
 const homeStyle = css`
   padding: 4rem 12rem;
@@ -21,6 +22,7 @@ const Home = () => {
       <Main /> {/* 메인 서비스 타이틀 */}
       {/* 문제 인식 */}
       <Section2 /> {/* 기능 소개 */}
+      <Fy /> {/* 파이 소개 */}
     </div>
   );
 };
