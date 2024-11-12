059c2953963e23414847977be700b8719f931998
hyuke81 <rmsidthgusdl81@gmail.com>
2024-11-13 00:46:00 +0900

    ✨ Feat: 베타 테스트 배너, 베타 테스트 참여 폼 페이지 Home페이지에 추가

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,22 +1,45 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
+import { useRef } from 'react';
 import Main from './Main';
 import Section2 from './Section2';
 import Fy from './Fy';
 import Feature from './Feature';
+import BetaTesteButton from './BetatestBanner';
+import JoinForm from './JoinForm';
 
 const homeStyle = css`
-
+  /* 홈 스타일을 정의할 수 있습니다. */
 `;
 
 const Home = () => {
+  const mainRef = useRef(null); 
+  const joinFormRef = useRef(null); 
+
+  const scrollToMain = () => {
+    if (mainRef.current) {
+      mainRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  const scrollToJoinForm = () => {
+    if (joinFormRef.current) {
+      joinFormRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <div css={homeStyle}>
-      <Main /> {/* 메인 서비스 타이틀 */}
-      {/* 문제 인식 */}
+      <div ref={mainRef}>
+        <Main /> {/* 메인 서비스 타이틀 */}
+      </div>
       <Section2 /> {/* 기능 소개 */}
+      <BetaTesteButton scrollToJoinForm={scrollToJoinForm} /> {/* 베타 테스트 배너 */}
       <Fy /> {/* 파이 소개 */}
       <Feature /> {/* 기능 상세 소개 */}
+      <div ref={joinFormRef}>
+        <JoinForm scrollToMain={scrollToMain} /> {/* 베타 테스트 참여 폼 */}
+      </div>
     </div>
   );
 };
