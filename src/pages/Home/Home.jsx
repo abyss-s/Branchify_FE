330e81b0441b8d6b3e9e516034d1baa609625598
abyss-s <xyz@inha.edu>
2024-11-17 16:08:34 +0900

    ♻️ Refactor(Home.jsx): 스크롤 액션 홈에서 등록 처리

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,44 +1,50 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { setScrollToJoinForm } from '../../stores/store';
 import Main from './Main';
 import Section2 from './Section2';
 import Fy from './Fy';
 import Feature from './Feature';
-import BetaTesteButton from './BetatestBanner/BetatestBanner';
+import BetaTestBanner from './BetatestBanner/BetatestBanner';
 import JoinForm from './JoinForm/JoinForm';
 
-const homeStyle = css`
-
-`;
+const homeStyle = css``;
 
 const Home = () => {
-  const mainRef = useRef(null); 
-  const joinFormRef = useRef(null); 
+  const dispatch = useDispatch();
+  const mainSectionRef = useRef(null);
+  const joinFormSectionRef = useRef(null);
 
-  const scrollToMain = () => {
-    if (mainRef.current) {
-      mainRef.current.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
+  useEffect(() => {
+    const scrollToJoinForm = () => {
+      if (joinFormSectionRef.current) {
+        joinFormSectionRef.current.scrollIntoView({ behavior: 'smooth' });
+      }
+    };
 
-  const scrollToJoinForm = () => {
-    if (joinFormRef.current) {
-      joinFormRef.current.scrollIntoView({ behavior: 'smooth' });
+    dispatch(setScrollToJoinForm(scrollToJoinForm)); // 스크롤 함수 Redux에 등록
+  }, [dispatch]);
+
+  const scrollToMainSection = () => {
+    if (mainSectionRef.current) {
+      mainSectionRef.current.scrollIntoView({ behavior: 'smooth' });
     }
   };
 
   return (
     <div css={homeStyle}>
-      <div ref={mainRef}>
+      <div ref={mainSectionRef}>
         <Main /> {/* 메인 서비스 타이틀 */}
       </div>
       <Section2 /> {/* 기능 소개 */}
-      <BetaTesteButton scrollToJoinForm={scrollToJoinForm} /> {/* 베타 테스트 배너 */}
+      <BetaTestBanner /> {/* 베타 테스트 배너 */}
       <Fy /> {/* 파이 소개 */}
       <Feature /> {/* 기능 상세 소개 */}
-      <div ref={joinFormRef}>
-        <JoinForm scrollToMain={scrollToMain} /> {/* 베타 테스트 참여 폼 */}
+      <div ref={joinFormSectionRef}>
+        <JoinForm scrollToMain={scrollToMainSection} />{' '}
+        {/* 베타 테스트 참여 폼 */}
       </div>
     </div>
   );
