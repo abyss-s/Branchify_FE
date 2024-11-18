507860a5cdd66860004f6e27a97f87563d63cd02
hyuke81 <rmsidthgusdl81@gmail.com>
2024-11-19 01:25:33 +0900

    ✅ Test: vercel의 프록시 설정 사용해 http요청 우회

--- src/pages/Home/JoinForm/Api.js
+++ src/pages/Home/JoinForm/Api.js
@@ -2,10 +2,7 @@ import axios from 'axios';
 
 export const submitEmail = async (email) => {
     try {
-        const response = await axios.post(
-            'http://101.101.217.98:8080/api/branchify/beta-tester',
-            { email }
-        );
+        const response = await axios.post('/api/branchify/beta-tester', { email });
         return response.data;
     } catch (error) {
         throw new Error('이메일 등록에 실패했습니다. 다시 시도해 주세요.');
