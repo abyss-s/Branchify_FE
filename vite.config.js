8c0fe075482cb73784aa46d39fd3dd5a94ba87ff
hyuke81 <rmsidthgusdl81@gmail.com>
2024-11-19 02:07:01 +0900

    ✅ Test: 프록시 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://101.101.217.98:8080', // 백엔드 서버 URL
+        changeOrigin: true, // 크로스 오리진 허용
+        rewrite: (path) => path.replace(/^\/api/, ''), // '/api'를 백엔드 경로에서 제거
+      },
+    },
+  },
 });
