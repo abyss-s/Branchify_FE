60626577df8b6aa4f04327d8b41d09dc93a7bdb9
hyuke81 <rmsidthgusdl81@gmail.com>
2024-11-25 13:49:27 +0900

    🐛 Fix: Api.js  서버경로 수정

--- src/pages/Home/JoinForm/Api.js
+++ src/pages/Home/JoinForm/Api.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+const SERVER = import.meta.env.VITE_SERVER_API;
+
 export const submitEmail = async (email) => {
     try {
-        const response = await axios.post('/api/branchify/beta-tester', { email });
+        const response = await axios.post('${SERVER}/api/branchify/beta-tester', { email });
         return response.data;
     } catch (error) {
         throw new Error('이메일 등록에 실패했습니다. 다시 시도해 주세요.');
