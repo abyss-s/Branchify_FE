c190b9bb7ef5353496478ba905ff204537b5e8fe
hyuke81 <rmsidthgusdl81@gmail.com>
2024-11-25 13:59:59 +0900

    🐛 Fix(Api.js): withCredentials 추가

--- src/pages/Home/JoinForm/Api.js
+++ src/pages/Home/JoinForm/Api.js
@@ -4,7 +4,10 @@ export const submitEmail = async (email) => {
     try {
         const response = await axios.post('https://www.branchify.site/api/branchify/beta-tester', {
         email: email,
-        });
+        },
+    {
+        withCredentials: true,
+    });
         return response.data;
     } catch (error) {
         throw new Error('이메일 등록에 실패했습니다. 다시 시도해 주세요.');
