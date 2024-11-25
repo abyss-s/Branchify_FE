60f996f599f2cb3c464972ee00daecfa3c2f16c3
abyss-s <xyz@inha.edu>
2024-11-25 12:32:20 +0900

    🚑️ Fix: api 설정 변경

--- src/pages/Home/JoinForm/Api.js
+++ src/pages/Home/JoinForm/Api.js
@@ -1,10 +1,14 @@
 import axios from 'axios';
 
+const SERVER = import.meta.env.VITE_SERVER_API;
+
 export const submitEmail = async (email) => {
-    try {
-        const response = await axios.post('/api/branchify/beta-tester', { email });
-        return response.data;
-    } catch (error) {
-        throw new Error('이메일 등록에 실패했습니다. 다시 시도해 주세요.');
-    }
+  try {
+    const response = await axios.post(`${SERVER}/api/branchify/beta-tester`, {
+      email: email,
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error('이메일 등록에 실패했습니다. 다시 시도해 주세요.');
+  }
 };
--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,19 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  const serverPort = Number(env.VITE_SERVER_DOMAIN_PORT);
+  const server = Number(env.VITE_SERVER_API);
 
   return {
     base: '/',
     plugins: [react()],
-    server: {
-      port: serverPort, // 서버 포트 설정
-      proxy: {
-        '/api': {
-          target: env.VITE_SERVER_DOMAIN_PORT, // 백엔드 서버 URL
-          changeOrigin: true, // 크로스 오리진 허용
-          rewrite: (path) => path.replace(/^\/api/, ''), // '/api'를 백엔드 경로에서 제거
-        },
-      },
-    },
+    // server: {
+    //   proxy: {
+    //     '/api': {
+    //       target: env.VITE_SERVER_API, // 백엔드 서버 URL
+    //       rewrite: (path) => path.replace(/^\/api/, ''), // '/api'를 백엔드 경로에서 제거
+    //     },
+    //   },
+    // },
     define: {
       __APP_ENV__: JSON.stringify(env.APP_ENV),
     },
